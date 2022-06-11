6b1713705dfa2076e95ab36e5a487ca4836e1686
pranay <mangal.pranay@gmail.com>
2022-06-11 18:51:34 +0530

    test1

---
+++ src/components/Accordian.js
@@ -0,0 +1,15 @@
+import React,{useState} from'react'
+
+export default function Accordian()
+{ const [count,setcount]=useState(0);
+    const 
+    return(
+<div>
+<button type="button" class="btn btn-secondary" onClick={handleclick}>Secondary</button>
+<h1>{count}</h1>
+    </div>
+    
+
+
+)
+}
